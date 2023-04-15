792c30973c708dd7ab78e379383bd0b395fe6690
jtsarvida <jtsarvida@gmail.com>
2023-04-15 14:31:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,13 +5,7 @@ class App < Sinatra::Base
     get '/' do
         erb :user_input
     end
-=begin
-    post '/piglatinize' do
-        piglatinizer = Piglatinizer.new
-        @result = piglatinizer.piglatinize(params[:user_phrase])
-        erb :results
-    end
-=end
+    
     post '/piglatinize' do
         piglatinizer = PigLatinizer.new
         @result = piglatinizer.piglatinize(params[:user_phrase])
